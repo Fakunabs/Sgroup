9d598f7562926aa994c7a3a28a7a6e0b20fdd2f7
Fakunabs <gempro0501@gmail.com>
2023-01-07 11:29:23 +0700

    From Mac

---
+++ Day 7/.history/script_20230107110242.js
@@ -0,0 +1,14 @@
+// Câu 1 : Viết hảm Javascript trả về chuỗi thông tin ngày giờ hiện tại
+function getDateTime() {
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    return `Now is: ${hour}:${minute}:${second} ${day}/${month}/${year}`
+}
+
+const now = getDateTime()
+console.log(now) // Now is: 23:30 28/12/2022
