2ea0e3d7ed90becfd691a6ac31bbd822cbfa6b9f
Fakunabs <gempro0501@gmail.com>
2023-02-05 12:56:32 +0700

    Todo List

--- Day 11/script.js
+++ Day 11/script.js
@@ -1,50 +1,64 @@
-
-// If the user write task into the input field and click submit or press enter key, the task will be added to the li tag in toDoList
-
 const form = document.querySelector(".form");
+const input = document.querySelector(".form__input");
 const toDoList = document.querySelector(".toDoList");
-const inputField = document.querySelector(".form__input");
-const clearButton = document.querySelector("#clear .button");
+const clearBtn = document.querySelector("#clear .clearBtn");
 
-form.addEventListener("submit", addTask);
-clearButton.addEventListener("click", clearList);
-
-function addTask(event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let inputValue = inputField.value;
-  let task = document.createElement("li");
-  task.innerHTML = inputValue;
-  toDoList.appendChild(task);
-  inputField.value = "";
-}
 
-function clearList() {
-  // toDoList.innerHTML = "";
-  let clear  = confirm("Do you want to clear the list?");
-  if (clear === true) {
-    toDoList.innerHTML = "";
+  if (input.value.trim() === "") {
+    alert("Please enter a task");
+    return;
   }
-}
 
-toDoList.addEventListener("click", toggleTaskCompletion);
+  const li = document.createElement("li");
+  li.innerHTML = `<span> ${input.value} 
+  <div class="icons">
+    <i class="far fa-check-circle circle"></i>
+    <i class="far fa-edit edit"></i>
+    <i class="far fa-trash-alt delete"></i>
+  </div>
+</span>`;
 
-function toggleTaskCompletion(event) {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("completed");
-  }
-}
-toDoList.addEventListener("dblclick", function(event) {
-  if (event.target.tagName === "LI") {
-    let confirmDelete = confirm("Are you sure you want to delete this task?");
-    if (confirmDelete === true) {
-      event.target.remove();
-    }
-  }
+  toDoList.appendChild(li);
+  addTaskListeners(li);
+  input.value = "";
 });
 
+const addTaskListeners = (task) => {
+  const checkButton = task.querySelector(".fa-check-circle");
+  checkButton.addEventListener("click", () => {
+    if (task.style.textDecoration === "line-through") {
+      task.style.textDecoration = "none";
+      task.style.color = "black";
+    } else {
+      task.style.textDecoration = "line-through";
+      task.style.color = "green";
+      alert("Task Completed");
+    }
+  });
 
+  const deleteBtn = task.querySelector(".fa-trash-alt");
+  deleteBtn.addEventListener("click", () => {
+    task.remove();
+  });
 
+  const editButton = task.querySelector(".fa-edit");
+  editButton.addEventListener("click", () => {
+    let taskText = task.querySelector("span");
+    let newTask = prompt("Enter new task:", taskText.textContent);
+    if (newTask) {
+      taskText.textContent = newTask;
+    }
+  });
+};
 
+const tasks = document.querySelectorAll("li");
+tasks.forEach((task) => {
+  addTaskListeners(task);
+});
 
-
-
+clearBtn.addEventListener("click", function () {
+  const toDoList = document.querySelector(".toDoList");
+  toDoList.innerHTML = "";
+});
